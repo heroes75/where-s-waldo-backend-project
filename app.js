a605f88338f96599cdb31b5e2e9ec8d7c8309d0c
heroes75 <123456789+odin@users.noreply.github.com>
2026-06-01 03:35:29 +0000

    advance

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const {createServer} = require('node:http')
 const { Server } = require('socket.io')
 const game = require('./routes/gameRouter')
 const leadboardRouter = require('./routes/leadboardRouter')
+const { randomUUID } = require('node:crypto')
 
 
 const app = express()
@@ -14,6 +15,8 @@ const io = new Server(server, {
         origin: ["http://localhost:5173/", 'http://127.0.0.1:5173']
     }
 })
+const lobby = new Set()
+const roomId = {id: ''}
 
 function totalCount() {
     return io.of("/").sockets.size;
@@ -24,16 +27,43 @@ function clientCountInMultiplayer(id) {
 }
 
 io.on('connection', (socket) => {
-    
+    console.log('user connected:')
+
     socket.join('multiplayer')
-    socket.on('multiplayer', (id, msg) => {
-        console.log('id:', id);
-        console.log('msg:', msg)
-        console.log('clientCountInMultiplayer', clientCountInMultiplayer('multiplayer'))
+    console.log('lobby:', lobby.size)
+    if (lobby.size  === 0) {
+        var id = randomUUID() + ''
+        roomId.id = id
+        socket.broadcast.emit('lobby', { msg: 'Please Wait...', nextGame: null, id})
+        socket.emit('lobby', { msg: 'Please Wait...', nextGame: null, id})
+        socket.join(id)
+        io.to(id).emit('welcome')
+        lobby.add(socket.id)
+        console.log('lobby:', lobby.size)
+    } else if(lobby.size  === 1) {
+        lobby.add(socket.id)
+        socket.broadcast.emit('lobby', {msg: 'you\'re connected now to a player', nextGame: '/multiplayer/' + roomId.id + '/cmpeul32y0000yyud7tymzxse', id: roomId.id})
+        socket.emit('lobby', {msg: 'you\'re connected now to a player', id: roomId.id, nextGame: '/multiplayer/' + roomId.id + '/cmpeul32y0000yyud7tymzxse', id: roomId.id})
+        socket.join(roomId.id)
+        io.to(roomId.id).emit('welcome')
+        console.log('lobby:', lobby.size)
+        // io.to(socket.id).emit('game', 'New Game');
+    }
+
+    socket.on('disconnect', () => {
+        console.log('disconnected:')
+        lobby.delete(socket.id)
     })
-    const ipAddress = socket.handshake.address
-    console.log(ipAddress)
-    console.log('totalCount', totalCount())
+    
+    // socket.join('multiplayer')
+    // socket.on('multiplayer', (id, msg) => {
+    //     console.log('id:', id);
+    //     console.log('msg:', msg)
+    //     console.log('clientCountInMultiplayer', clientCountInMultiplayer('multiplayer'))
+    // })
+    // const ipAddress = socket.handshake.address
+    // console.log(ipAddress)
+    // console.log('totalCount', totalCount())
 })
 app.use(cors())
 app.use(express.json());
