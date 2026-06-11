d4941bb2e1e8144acaf8ec055fc31b3b4e1efaa7
heroes75 <123456789+odin@users.noreply.github.com>
2026-06-11 03:09:01 +0000

    add method to cors

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const io = new Server(server, {
     cors: {
         origin: ["http://localhost:5173/", 'http://127.0.0.1:5173', 'https://where-s-waldo-frontend-project.vercel.app', process.env.FRONTEND_API_1, process.env.FRONTEND_API_2]
     },
+    method: ['GET', 'POST'],
 })
 const lobby = new Set()
 const roomId = {id: ''}
